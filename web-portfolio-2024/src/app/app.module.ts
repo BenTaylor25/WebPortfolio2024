65018774791af9c3f331e2facff3a49b2743cc6d
Ben Taylor <ben.taylor2573@gmail.com>
2024-01-07 21:42:32 +0000

    generated components

--- web-portfolio-2024/src/app/app.module.ts
+++ web-portfolio-2024/src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { MainPageComponent } from './main-page/main-page.component';
+import { HeroComponent } from './main-page/hero/hero.component';
+import { BioComponent } from './main-page/bio/bio.component';
+import { WorkComponent } from './main-page/work/work.component';
+import { ProjectsComponent } from './main-page/projects/projects.component';
+import { EducationComponent } from './main-page/education/education.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MainPageComponent,
+    HeroComponent,
+    BioComponent,
+    WorkComponent,
+    ProjectsComponent,
+    EducationComponent
   ],
   imports: [
     BrowserModule,
