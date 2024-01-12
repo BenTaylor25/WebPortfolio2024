804cdaca356b8ad25cbfec2c572fcfbe5f7aeb45
benlaptop25 <ben.taylor2573@gmail.com>
2024-01-12 22:32:38 +0000

    basic routing

--- web-portfolio-2024/src/app/app-routing.module.ts
+++ web-portfolio-2024/src/app/app-routing.module.ts
@@ -1,7 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HeaderComponent } from './header/header.component';
+import { MainPageComponent } from './main-page/main-page.component';
+import { HeroComponent } from './main-page/hero/hero.component';
+import { BioComponent } from './main-page/bio/bio.component';
+import { WorkComponent } from './main-page/work/work.component';
+import { ProjectsComponent } from './main-page/projects/projects.component';
+import { EducationComponent } from './main-page/education/education.component';
+
+export const routingComponents = [
+  HeaderComponent,
+  MainPageComponent,
+  HeroComponent,
+  BioComponent,
+  WorkComponent,
+  ProjectsComponent,
+  EducationComponent
+];
+
+const routes: Routes = [
+  { path: '', component: MainPageComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- web-portfolio-2024/src/app/app.module.ts
+++ web-portfolio-2024/src/app/app.module.ts
@@ -1,26 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { MainPageComponent } from './main-page/main-page.component';
-import { HeroComponent } from './main-page/hero/hero.component';
-import { BioComponent } from './main-page/bio/bio.component';
-import { WorkComponent } from './main-page/work/work.component';
-import { ProjectsComponent } from './main-page/projects/projects.component';
-import { EducationComponent } from './main-page/education/education.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    MainPageComponent,
-    HeroComponent,
-    BioComponent,
-    WorkComponent,
-    ProjectsComponent,
-    EducationComponent
+    routingComponents
   ],
   imports: [
     BrowserModule,
