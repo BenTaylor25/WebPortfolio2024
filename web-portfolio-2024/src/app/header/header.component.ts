99a5d9937eb80011a2903ffa3ac0ba52f83baa0a
benlaptop25 <ben.taylor2573@gmail.com>
2024-01-31 23:30:25 +0000

    hide hamburger on wide screens

--- web-portfolio-2024/src/app/header/header.component.ts
+++ web-portfolio-2024/src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ResponsiveService } from '../responsive.service';
 
 @Component({
   selector: 'app-header',
@@ -6,7 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
+  isMobile = false;
   isHamburgerActive = false;
+
+  constructor(private responsiveService: ResponsiveService) {}
+
+  ngOnInit(): void {
+    this.responsiveService.getIsMobile().subscribe(
+      isMobile => this.isMobile = isMobile
+    );
+  }
 
   toggleHamburger() {
     this.isHamburgerActive = !this.isHamburgerActive;
---
+++ web-portfolio-2024/src/app/responsive.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ResponsiveService {
+  private isMobile = new BehaviorSubject<boolean>(false);
+
+  constructor() {
+    this.checkScreenWidth();
+    window.addEventListener('resize', () => this.checkScreenWidth());
+  }
+
+  private checkScreenWidth(): void {
+    const isMobile = window.innerWidth < 600;
+    this.isMobile.next(isMobile);
+  }
+
+  getIsMobile(): Observable<boolean> {
+    return this.isMobile.asObservable();
+  }
+}
